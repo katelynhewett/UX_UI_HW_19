976ae5b1a64d76db6ef89afdfe5d1ad5e5b986a6
katelynhewett <katelynhewett@gmail.com>
2021-11-11 17:26:13 -0600

    Jquery updates 11.11

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$("#learnMoreButton").hover(function(){
+      $(this).css("background-color","#D15742");
+    }, function() {
+      $(this).css("background-color","#3D4F66");
+    }
+  );
+
+
+
+$(".caseStudyButton").hover(function(){
+    $(this).css("background-color","#D15742");
+    $(this).css("color","#FBFAF8");
+    $(this).css("border-color","#FBFAF8");
+  }, function() {
+    $(this).css("background-color","#FBFAF8");
+    $(this).css("color","#2F2F31");
+    $(this).css("border-color","#2F2F31");
+  }
+);
