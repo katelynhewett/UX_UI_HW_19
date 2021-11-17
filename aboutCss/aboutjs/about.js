3eb644ef5387d636cf492d5978882a990dee476e
katelynhewett <katelynhewett@gmail.com>
2021-11-16 22:47:31 -0600

    Updates 11.16

---
+++ aboutCss/aboutjs/about.js
@@ -0,0 +1,24 @@
+
+
+$(".facebookIcon").hover(function(){
+$(this).attr("src","images/facebookRed.png");
+}, function() {
+$(this).attr("src","images/facebookIcon.png");
+}
+);
+
+
+$(".linkedInIcon").hover(function(){
+$(this).attr("src","images/linkedinRed.png");
+}, function() {
+$(this).attr("src","images/linkedInIcon.png");
+}
+);
+
+
+$(".instagramLogo").hover(function(){
+$(this).attr("src","images/instagramRed.png");
+}, function() {
+$(this).attr("src","images/instagramIcon.png");
+}
+);
--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 $("#learnMoreButton").hover(function(){
-      $(this).css("background-color","#D15742");
+      $(this).css("background-color","#C75644");
     }, function() {
       $(this).css("background-color","#3D4F66");
     }
   );
 
 
-
 $(".caseStudyButton").hover(function(){
-    $(this).css("background-color","#D15742");
+    $(this).css("background-color","#C75644");
     $(this).css("color","#FBFAF8");
     $(this).css("border-color","#FBFAF8");
   }, function() {
@@ -16,4 +15,66 @@ $(".caseStudyButton").hover(function(){
     $(this).css("color","#2F2F31");
     $(this).css("border-color","#2F2F31");
   }
+);
+
+$(".connectButton").hover(function(){
+  $(this).css("background-color","#C75644");
+}, function() {
+  $(this).css("background-color","#3D4F66");
+}
+);
+
+
+$(".caseStudiesNav").hover(function(){
+  $(this).css("color","#C75644");
+}, function() {
+  $(this).css("color","#050601");
+}
+);
+
+
+$(".resumeNav").hover(function(){
+  $(this).css("color","#C75644");
+}, function() {
+  $(this).css("color","#050601");
+}
+);
+
+
+$(".contactNav").hover(function(){
+  $(this).css("color","#C75644");
+}, function() {
+  $(this).css("color","#050601");
+}
+);
+
+
+$(".aboutNavigation").hover(function(){
+  $(this).css("color","#C75644");
+}, function() {
+  $(this).css("color","#050601");
+}
+);
+
+$(".facebookIcon").hover(function(){
+  $(this).attr("src","images/facebookRed.png");
+}, function() {
+  $(this).attr("src","images/facebookIcon.png");
+}
+);
+
+
+$(".linkedInIcon").hover(function(){
+  $(this).attr("src","images/linkedinRed.png");
+}, function() {
+  $(this).attr("src","images/linkedInIcon.png");
+}
+);
+
+
+$(".instagramLogo").hover(function(){
+  $(this).attr("src","images/instagramRed.png");
+}, function() {
+  $(this).attr("src","images/instagramIcon.png");
+}
 );
